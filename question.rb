143876344e576c559e63fb4934470f570579cc22
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-10-04 13:02:32 -0700

    Add @player_answer and check_answer method

--- question.rb
+++ question.rb
@@ -1,13 +1,23 @@
 class Question
   attr_reader :num1, :num2, :answer
+  attr_writer :player_answer
 
   def initialize
     @num1 = rand(1..20)
     @num2 = rand(1..20)
     @answer = @num1 + @num2
+    @player_answer = nil
   end
 
-  def question
+  def prompt
     "What does #{@num1} plus #{@num2} equal?"
+  end
+
+  def check_answer
+    if @player_answer == @answer
+      true
+    else
+      false
+    end
   end
 end
